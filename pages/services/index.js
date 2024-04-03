92f2557b86baca43dcf962502eccedd516d08328
DartleDartle <davide.pintore98@gmail.com>
2024-04-03 15:44:37 +0200

    fixing skills page

--- pages/services/index.js
+++ pages/services/index.js
@@ -9,11 +9,11 @@ import { fadeIn } from '../../variants';
 
 const Services = () => {
   return (
-    <div className='min-h-screen bg-grey py-36 xl:pt-0 xl:mt-0 flex items-center'>
+    <div className='min-h-screen bg-grey flex items-center'>
       <div className='container mx-auto '>
         <div className='flex flex-col xl:flex-row gap-x-8'>
           {/* text */}
-          <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
+          <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0 xl:mt-12'>
             <motion.h2
               variants={fadeIn('up', 0.2)}
               initial='hidden'
--- pages/work/index.js
+++ pages/work/index.js
@@ -7,12 +7,12 @@ import { fadeIn } from '../../variants';
 
 const Work = () => {
   return (
-    <div className='h-screen bg-grey py-36 flex items-center'>
+    <div className='min-h-screen bg-grey py-9 flex items-center'>
       
       <div className='container mx-auto'>
-        <div className='flex flex-col xl:flex-row gap-x-8'>
+        <div className='flex flex-col xl:flex-row gap-x-8  '>
           {/* text */}
-          <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
+          <div className='text-center flex xl:w-[30vw] flex-col lg:text-left  mb-4 xl:mb-0 '>
             <motion.h2
               variants={fadeIn('up', 0.2)}
               initial='hidden'
@@ -40,7 +40,7 @@ const Work = () => {
             initial='hidden'
             animate='show'
             exit='hidden'
-            className='w-full xl:max-w-[65%]'
+            className='w-full xl:max-w-[65%] md:max-w-[75%]'
           >
             <WorkSlider />
           </motion.div>
